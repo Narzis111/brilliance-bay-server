a0aeb33997d9249d1f48ead1ea5da9fa01ab15cd
Narzis111 <narzisnahar111@gmail.com>
2024-06-02 09:13:36 +0100

    middlewares verifytoken created

--- index.js
+++ index.js
@@ -44,32 +44,32 @@ async function run() {
 //     })
 
 //     // middlewares 
-//     const verifyToken = (req, res, next) => {
-//       // console.log('inside verify token', req.headers.authorization);
-//       if (!req.headers.authorization) {
-//         return res.status(401).send({ message: 'unauthorized access' });
-//       }
-//       const token = req.headers.authorization.split(' ')[1];
-//       jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (error, decoded) => {
-//         if (error) {
-//           return res.status(401).send({ message: 'unauthorized access' })
-//         }
-//         req.decoded = decoded;
-//         next();
-//       })
-//     }
+    const verifyToken = (req, res, next) => {
+      // console.log('inside verify token', req.headers.authorization);
+      if (!req.headers.authorization) {
+        return res.status(401).send({ message: 'unauthorized access' });
+      }
+      const token = req.headers.authorization.split(' ')[1];
+      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (error, decoded) => {
+        if (error) {
+          return res.status(401).send({ message: 'unauthorized access' })
+        }
+        req.decoded = decoded;
+        next();
+      })
+    }
 
 //     // use verify admin after verifyToken
-//     const verifyAdmin = async (req, res, next) => {
-//       const email = req.decoded.email;
-//       const query = { email: email };
-//       const user = await userCollection.findOne(query);
-//       const isAdmin = user?.role === 'admin';
-//       if (!isAdmin) {
-//         return res.status(403).send({ message: 'forbidden access' });
-//       }
-//       next();
-//     }
+    const verifyAdmin = async (req, res, next) => {
+      const email = req.decoded.email;
+      const query = { email: email };
+      const user = await userCollection.findOne(query);
+      const isAdmin = user?.role === 'admin';
+      if (!isAdmin) {
+        return res.status(403).send({ message: 'forbidden access' });
+      }
+      next();
+    }
 
 //     //   users related api. all user data only admin pabe
 //     app.get('/users', verifyToken, verifyAdmin, async (req, res) => {
